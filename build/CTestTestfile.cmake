a4fcce330fc3c45d5a6c7fdd5aef16e7a7eb1d11
tejal <tezzb18@gmail.com>
2020-09-02 17:21:53 +0530

    added

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tejal/Obstacle_avoidance-bot-using-ROS-and-GAZEBO/src
+# Build directory: /home/tejal/Obstacle_avoidance-bot-using-ROS-and-GAZEBO/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(path_planning)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/tejal/Obstacle_avoidance-bot-using-ROS-and-GAZEBO/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/tejal/Obstacle_avoidance-bot-using-ROS-and-GAZEBO/devel;/home/tejal/ros20_workshop_codes/ros20_ws/devel;/home/tejal/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/tejal/Obstacle_avoidance-bot-using-ROS-and-GAZEBO/devel;/home/tejal/ros20_workshop_codes/ros20_ws/devel;/home/tejal/catkin_ws/devel;/opt/ros/kinetic" != "" else []
