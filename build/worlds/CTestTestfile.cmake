043b54ad0f713b6ffe46171dd8069937b007de58
tejal <tezzb18@gmail.com>
2020-09-06 19:32:54 +0530

    added

---
+++ build/worlds/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tejal/Obstacle_avoidance-bot-using-ROS-and-GAZEBO/src/worlds
+# Build directory: /home/tejal/Obstacle_avoidance-bot-using-ROS-and-GAZEBO/build/worlds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
